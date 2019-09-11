23a9c5042f7b2593835ee0be0019a787481b93f7
arshak <arshakr10@gmail.com>
2019-09-11 22:22:48 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you");
+            int age = Convert.ToInt32(Console.ReadLine());
+            {
+                if (age < 23) 
+                Console.WriteLine("you are so young");
+                if  (age >= 23) 
+                Console.WriteLine("Can we meet ? ))");
+                Console.WriteLine($" {name},  ");
+                Console.ReadKey(); 
+
+                    
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
