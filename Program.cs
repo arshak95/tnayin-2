decf63bcec0d1c03b0aeb97dd03cef0949b1a6f0
arshak <arshakr10@gmail.com>
2019-09-15 16:01:25 +0300

    new code

--- Program.cs
+++ Program.cs
@@ -8,40 +8,31 @@ namespace New_Project
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[]args )
+
         {
-            Console.WriteLine("what is your name");
-            string name = Console.ReadLine();
-            Console.WriteLine("How old are you");
-            int age = Convert.ToInt32(Console.ReadLine());
-            {
-                if (age < 23) 
-                Console.WriteLine("you are so young");
-                if  (age >= 23) 
-                Console.WriteLine("Can we meet ? ))");
-                Console.WriteLine($" {name},  ");
-                Console.ReadKey(); 
+            const Double a = 1.0; 
+            const Double b = 4.0;
+            const Double c = 3.0;
+            Double x1;
+            Double x2;
 
-                    
+           Double D= Math.Pow(b, 2) - 4 * a * c;
+           if (D > 0 || D == 0) 
+        {
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
 
+                Console.WriteLine(x1);
+                Console.WriteLine(x2);
 
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          
 
 
         }
     }
 }
+
+
+
